6faa27edb46850ba8ad28b12d456cd20255ec588
Franz Flasch <franz.flasch@gmx.at>
2016-09-23 21:48:54 +0200

    Added crc32 function

--- crc.c
+++ crc.c
@@ -12,3 +12,23 @@ uint16_t crc16_ccitt(uint8_t *data_p, uint8_t length)
     }
     return crc;
 }
+
+
+uint32_t crc32(uint32_t previousCrc32, const void* data, size_t length)
+{
+  uint32_t crc = ~previousCrc32; // same as previousCrc32 ^ 0xFFFFFFFF
+  unsigned char* current = (unsigned char*) data;
+  while (length--)
+  {
+    crc ^= *current++;
+    for (unsigned int j = 0; j < 8; j++)
+    {
+      if (crc & 1)
+        crc = (crc >> 1) ^ 0xEDB88320;
+      else
+        crc =  crc >> 1;
+    }
+  }
+  return ~crc; // same as crc ^ 0xFFFFFFFF
+}
+
--- crc.h
+++ crc.h
@@ -4,5 +4,6 @@
 #include <stdint.h>
 
 uint16_t crc16_ccitt(uint8_t *data_p, uint8_t length);
+uint32_t crc32(uint32_t previousCrc32, const void* data, size_t length);
 
 #endif /* CRC_CCITT_H_ */
